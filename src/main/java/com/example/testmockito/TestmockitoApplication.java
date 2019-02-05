727a4e9dcaaa3a79f4b35eccc8b3413bc8048a69
Thiago Chagas <thiago.chagas@dextra-sw.com>
2019-02-05 17:25:50 -0200

    Spring boot project

---
+++ src/main/java/com/example/testmockito/TestmockitoApplication.java
@@ -0,0 +1,14 @@
+package com.example.testmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmockitoApplication.class, args);
+	}
+
+}
+
